2a9d2755370ed0024bd2cbcdd56dcca9aec075d8
Imisi Bailey <imisibailey@gmail.com>
2024-08-26 15:09:38 +0100

    created a random password generator

---
+++ Random-password-generator.py
@@ -0,0 +1,38 @@
+import string
+import random
+
+
+
+characters = list(string.ascii_letters + string.digits + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-=<>?:|][?}{|")
+
+def generate_password():
+    
+    password_length = int(input("How long do you want your password to be? : "))
+    
+    
+    random.shuffle(characters)
+    
+    password = []
+    
+    for x in range(password_length):
+        password.append(random.choice(characters))
+    random.shuffle
+    password = "".join(password)
+    
+    print(password)
+    
+    
+    
+    
+option = input("Do you want to generate a password? (yes/no): ")
+
+if option == "yes".lower():
+    generate_password()
+elif option == "no".lower():
+    print()
+    print("program ended")
+    quit()
+else:
+    print("Invalid word, option is: yes/no")
+    quit()
+
